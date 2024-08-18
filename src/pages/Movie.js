868e5873361c92534ee38e7e473956e81ab07675
Tracy Kemboi <tkemboi72@gmail.com>
2024-08-18 16:18:13 +0300

    Updated the components and pages

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,27 @@
-import { useEffect, useState } from "react";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
 function Movie() {
+  const { id } = useParams();
+  
+  // Assume you have a way to fetch movie data based on id
+  const movie = {
+    title: "Doctor Strange",
+    time: 115,
+    genres: ["Action", "Adventure", "Fantasy"]
+  };
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>{movie.title}</h1>
+      <p>{movie.time} minutes</p>
+      {movie.genres.map((genre, index) => (
+        <span key={index}>{genre} </span>
+      ))}
+    </div>
   );
-};
+}
 
 export default Movie;
